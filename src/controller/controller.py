0cc2b5b2fd9db2b410d5884f3f59e15c375877f2
Adam Blakney <akblakney@gmail.com>
2026-07-20 20:46:50 -0400

    zoom plot

--- src/controller/controller.py
+++ src/controller/controller.py
@@ -44,6 +44,7 @@ def create_app(serve_service: ServeService, pool_service: PoolService, stats_ser
             raise HTTPException(status_code=400, detail=str(e))
 
         plot_base64 = render_audio_plot_base64(bits_response.audio_samples) if plot else None
+        zoom_plot = render_audio_plot_base64(bits_response.audio_samples[:min(1000, len(bits_response.audio_samples))]) if plot else None
 
         logger.info('Exiting /bits')
 
@@ -51,6 +52,7 @@ def create_app(serve_service: ServeService, pool_service: PoolService, stats_ser
             "num_bytes": num_bytes,
             "bytes": bits_response.data.hex(),
             "audio_plot_png_base64": plot_base64,
+            "zoom_plog": zoom_plot,
             "pairs_discarded": bits_response.pairs_discarded,
             "pairs_kept": bits_response.pairs_kept,
             "total_chunks": bits_response.total_chunks
